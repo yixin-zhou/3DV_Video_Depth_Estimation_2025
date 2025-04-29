9e8d49ac1344ab51d82316ee188d86a89e88f296
yixin zhou <zhouyixin1006@163.com>
2025-04-29 17:21:30 +0200

    Push by Yixin Zhou

--- scripts/download_Sintel.py
+++ scripts/download_Sintel.py
@@ -3,6 +3,15 @@ import zipfile
 import os
 
 Sintel_url = 'http://files.is.tue.mpg.de/jwulff/sintel/MPI-Sintel-stereo-training-20150305.zip'
+save_dir = '../datasets'
+save_path = os.path.join(save_dir, 'MPI-Sintel-stereo-training-20150305.zip')
+extract_path = os.path.join(save_dir, 'MPI-Sintel-stereo-training-20150305')
 
-download_url(Sintel_url, save_path='../datasets/MPI-Sintel-stereo-training-20150305.zip', desc='Downloading Sintel Stereo Dataset')
+download_url(Sintel_url, save_path=save_path, desc='Downloading Sintel Stereo Dataset')
 
+with zipfile.ZipFile(save_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
+
+os.remove(save_path)
+
+print(f'Successfully download Sintel Dataset to {extract_path}')
